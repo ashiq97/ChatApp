53f80c8f8409264f60533a50a54aad8a37fe9ea5
ashiq97 <ashiq070shishir@gmail.com>
2023-05-30 17:47:19 +0600

    some updated

---
+++ clientApp/src/app/_models/members.ts
@@ -0,0 +1,17 @@
+export interface Member {
+  id: number
+  username: string
+  photoUrl: string
+  age: number
+  knownAs: string
+  created: Date
+  lastActive: Date
+  gender: string
+  introduction: string
+  interests: string
+  city: string
+  country: string
+  photos: Photo[]
+}
+
+
